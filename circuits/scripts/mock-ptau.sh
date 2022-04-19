4a153d76bc167846e6de0251790ad11f13e41fd3
CDDelta <me@cddelta.dev>
2022-04-19 13:23:39 +0000

    chore: add scripts ptau and groth16 setup

--- circuits/scripts/mock-ptau.sh
+++ circuits/scripts/mock-ptau.sh
@@ -1,3 +1,5 @@
-snarkjs powersoftau new bn128 15 pot15_0000.ptau -v
+snarkjs powersoftau new bn128 15 ./dist/pot15_0000.ptau -v
 
-snarkjs powersoftau contribute pot15_0000.ptau pot15_0001.ptau --name=mock-contrib -v -e="$(head -n 4096 /dev/urandom | openssl sha1)"
+snarkjs powersoftau contribute ./dist/pot15_0000.ptau ./dist/pot15_0001.ptau --name=mock-contrib -v -e="$(head -n 4096 /dev/urandom | openssl sha1)"
+
+snarkjs powersoftau prepare phase2 ./dist/pot15_0001.ptau ./dist/pot15_final.ptau -v
