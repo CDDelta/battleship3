b56920da00c3aaeaaebf909ff4087866acc9052f
CDDelta <me@cddelta.dev>
2022-04-19 13:44:18 +0000

    chore: add witness and proving scripts

---
+++ circuits/scripts/generate-proofs.sh
@@ -0,0 +1,3 @@
+snarkjs groth16 prove ./dist/boardsetup_0001.zkey boardsetup.wtns boardsetup-proof.json boardsetup-public.json
+
+snarkjs groth16 prove ./dist/fireshot_0001.zkey fireshot.wtns fireshot-proof.json fireshot-public.json
---
+++ circuits/scripts/generate-witnesses.sh
@@ -0,0 +1,3 @@
+node dist/boardsetup_js/generate_witness.js dist/boardsetup_js/boardsetup.wasm boardsetup-input.json boardsetup.wtns
+
+node dist/fireshot_js/generate_witness.js dist/fireshot_js/fireshot.wasm fireshot-input.json fireshot.wtns
