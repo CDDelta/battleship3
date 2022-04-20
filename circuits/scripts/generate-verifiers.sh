d34b239d7a921204f71a42e612fb10b94ca0820a
CDDelta <me@cddelta.dev>
2022-04-20 04:12:16 +0000

    chore: rename generated verifier contracts

--- circuits/scripts/generate-verifiers.sh
+++ circuits/scripts/generate-verifiers.sh
@@ -1,3 +1,5 @@
 snarkjs zkey export solidityverifier ./dist/boardsetup_0001.zkey ../contracts/src/BoardSetupVerifier.sol
+sed -i -e 's/Verifier/BoardSetupVerifier/g' ../contracts/src/BoardSetupVerifier.sol
 
 snarkjs zkey export solidityverifier ./dist/fireshot_0001.zkey ../contracts/src/FireShotVerifier.sol
+sed -i -e 's/Verifier/FireShotVerifier/g' ../contracts/src/FireShotVerifier.sol
