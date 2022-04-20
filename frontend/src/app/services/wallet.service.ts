77d458454fe50b4812ef1a553433c782e1fca5d7
CDDelta <me@cddelta.dev>
2022-04-20 13:23:50 +0000

    feat: allow user to connect Metamask

---
+++ frontend/src/app/services/wallet.service.ts
@@ -0,0 +1,35 @@
+import { DOCUMENT } from '@angular/common';
+import { Inject, Injectable } from '@angular/core';
+import { JsonRpcSigner, Web3Provider } from '@ethersproject/providers';
+import { debounceTime, delay, map, Observable, Subject, switchMap, tap, throttleTime } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class WalletService {
+  provider: Web3Provider;
+
+  signer$: Observable<JsonRpcSigner>;
+  address$: Observable<string | undefined>;
+
+  private accountsChange$ = new Subject();
+
+  constructor(@Inject(DOCUMENT) document: Document) {
+    const window = document.defaultView as any;
+    this.provider = new Web3Provider(window.ethereum);
+
+    this.signer$ = this.accountsChange$.pipe(map(() => this.provider.getSigner()));
+    this.address$ = this.signer$.pipe(switchMap((signer) => signer.getAddress().catch(() => undefined)));
+
+    setTimeout(() => this.accountsChange$.next(null), 500);
+
+    window.ethereum.on('connect', () => this.accountsChange$.next(null));
+    window.ethereum.on('disconnect', () => this.accountsChange$.next(null));
+    window.ethereum.on("accountsChanged", () => this.accountsChange$.next(null));
+  }
+
+  async connect(): Promise<void> {
+    await this.provider.send("eth_requestAccounts", []);
+    this.accountsChange$.next(null)
+  }
+}
