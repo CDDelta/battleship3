f66ea2b2f9b7d3d350e070f25a6cc4b67a423d14
CDDelta <me@cddelta.dev>
2022-04-23 03:19:36 +0000

    chore: add proof validation scripts

---
+++ circuits/scripts/verify-proofs.sh
@@ -0,0 +1,3 @@
+snarkjs groth16 verify ./boardsetup_verification_key.json boardsetup-public.json boardsetup-proof.json
+
+snarkjs groth16 verify ./fireshot_verification_key.json fireshot-public.json fireshot-proof.json
