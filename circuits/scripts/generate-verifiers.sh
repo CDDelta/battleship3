22d8efd7b3aebe8ee72bb2701268f822db6c366c
CDDelta <me@cddelta.dev>
2022-04-23 12:59:46 +0000

    chore: generate verifier contracts during build

--- circuits/scripts/generate-verifiers.sh
+++
@@ -1,5 +0,0 @@
-snarkjs zkey export solidityverifier ./boardsetup_final.zkey ../contracts/src/BoardSetupVerifier.sol
-sed -i -e 's/Verifier/BoardSetupVerifier/g' ../contracts/src/BoardSetupVerifier.sol
-
-snarkjs zkey export solidityverifier ./fireshot_final.zkey ../contracts/src/FireShotVerifier.sol
-sed -i -e 's/Verifier/FireShotVerifier/g' ../contracts/src/FireShotVerifier.sol
