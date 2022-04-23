d65ef2cb8183ea1fa48d4cdd4436a3278dfa2fa7
CDDelta <me@cddelta.dev>
2022-04-23 14:00:39 +0000

    ci: add deployment action

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,3 +1,7 @@
 export const environment = {
-  production: true
+  production: true,
+  gqlQueryUri: 'http://localhost:8000/subgraphs/name/harmony/battleship3',
+  gqlSubscriptionUri:
+    'http://localhost:8001/subgraphs/name/harmony/battleship3',
+  gameContractAddress: '0xfb4ed1886deb4f1ff328bc5ac30240b5074fe8c8',
 };
