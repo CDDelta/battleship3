075f3af1e11f7d13fb8fd3169178fb8ca43813ff
CDDelta <me@cddelta.dev>
2022-04-23 06:45:52 +0000

    chore: export proof verification keys as part of groth16 setup

--- circuits/scripts/generate-verifiers.sh
+++ circuits/scripts/generate-verifiers.sh
@@ -1,7 +1,5 @@
-snarkjs zkey export verificationkey ./boardsetup_final.zkey ./boardsetup_verification_key.json
 snarkjs zkey export solidityverifier ./boardsetup_final.zkey ../contracts/src/BoardSetupVerifier.sol
 sed -i -e 's/Verifier/BoardSetupVerifier/g' ../contracts/src/BoardSetupVerifier.sol
 
-snarkjs zkey export verificationkey ./fireshot_final.zkey ./fireshot_verification_key.json
 snarkjs zkey export solidityverifier ./fireshot_final.zkey ../contracts/src/FireShotVerifier.sol
 sed -i -e 's/Verifier/FireShotVerifier/g' ../contracts/src/FireShotVerifier.sol
