9d3a1bfb1ae707a9e641da3f0c467fa9307a4dae
CDDelta <me@cddelta.dev>
2022-04-23 12:46:33 +0000

    chore: use hermez ptau ceremony instead of own mock

--- circuits/scripts/compile-circuits.sh
+++ circuits/scripts/compile-circuits.sh
@@ -1,3 +1,5 @@
+mkdir -p ./dist
+
 circom src/boardsetup.circom --r1cs --wasm --sym --c -o dist
 
 circom src/fireshot.circom --r1cs --wasm --sym --c -o dist
---
+++ circuits/scripts/download-ptau.sh
@@ -0,0 +1,1 @@
+wget -O powersOfTau28_hez_final_15.ptau https://hermez.s3-eu-west-1.amazonaws.com/powersOfTau28_hez_final_15.ptau
