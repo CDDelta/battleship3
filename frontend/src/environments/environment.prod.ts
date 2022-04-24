4d75f0b2555fa28ec50fc1757e2baa996a65ebb6
undefined <undefined>
2022-04-24 06:14:05 +0000

    fix prod

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
-  gqlQueryUri: 'http://localhost:8000/subgraphs/name/harmony/battleship3',
+  gqlQueryUri: 'http://35.224.237.39:8000/subgraphs/name/harmony/battleship3',
   gqlSubscriptionUri:
-    'http://localhost:8001/subgraphs/name/harmony/battleship3',
+    'http://35.224.237.39:8001/subgraphs/name/harmony/battleship3',
   gameContractAddress: '0x7a7b3a1f88d07d4aaa481474ee1853d715c1a70e',
 };
