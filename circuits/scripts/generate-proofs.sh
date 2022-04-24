d7847f32f2cde880d8be71531ce7da5be1892765
CDDelta <me@cddelta.dev>
2022-04-24 00:32:14 +0000

    chore: revert to Groth16

--- circuits/scripts/generate-proofs.sh
+++ circuits/scripts/generate-proofs.sh
@@ -1,3 +1,3 @@
-snarkjs plonk prove ./boardsetup_final.zkey boardsetup.wtns boardsetup-proof.json boardsetup-public.json
+snarkjs groth16 prove ./boardsetup_final.zkey boardsetup.wtns boardsetup-proof.json boardsetup-public.json
 
-snarkjs plonk prove ./fireshot_final.zkey fireshot.wtns fireshot-proof.json fireshot-public.json
+snarkjs groth16 prove ./fireshot_final.zkey fireshot.wtns fireshot-proof.json fireshot-public.json
--- circuits/scripts/setup-circuits.sh
+++ circuits/scripts/setup-circuits.sh
@@ -1,5 +1,9 @@
-snarkjs plonk setup ./dist/boardsetup.r1cs ./powersOfTau28_hez_final_15.ptau ./boardsetup_final.zkey
+snarkjs groth16 setup ./dist/boardsetup.r1cs ./powersOfTau28_hez_final_15.ptau ./dist/boardsetup_0000.zkey
+snarkjs zkey contribute ./dist/boardsetup_0000.zkey ./boardsetup_final.zkey --name=mock-contrib -v -e="$(head -n 4096 /dev/urandom | openssl sha1)"
+snarkjs zkey verify ./dist/boardsetup.r1cs ./powersOfTau28_hez_final_15.ptau ./boardsetup_final.zkey
 snarkjs zkey export verificationkey ./boardsetup_final.zkey ./boardsetup_verification_key.json
 
-snarkjs plonk setup ./dist/fireshot.r1cs ./powersOfTau28_hez_final_15.ptau ./fireshot_final.zkey
+snarkjs groth16 setup ./dist/fireshot.r1cs ./powersOfTau28_hez_final_15.ptau ./dist/fireshot_0000.zkey
+snarkjs zkey contribute ./dist/fireshot_0000.zkey ./fireshot_final.zkey --name=mock-contrib -v -e="$(head -n 4096 /dev/urandom | openssl sha1)"
+snarkjs zkey verify ./dist/fireshot.r1cs ./powersOfTau28_hez_final_15.ptau ./fireshot_final.zkey
 snarkjs zkey export verificationkey ./fireshot_final.zkey ./fireshot_verification_key.json
