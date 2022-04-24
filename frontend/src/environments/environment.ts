cd83fece6e1240104901b498607a130f6ff22879
undefined <undefined>
2022-04-24 05:44:42 +0000

    chore: use own deployed Graph instance

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -4,9 +4,9 @@
 
 export const environment = {
   production: false,
-  gqlQueryUri: 'http://localhost:8000/subgraphs/name/harmony/battleship3',
+  gqlQueryUri: 'http://35.224.237.39:8000/subgraphs/name/harmony/battleship3',
   gqlSubscriptionUri:
-    'http://localhost:8001/subgraphs/name/harmony/battleship3',
+    'http://35.224.237.39:8001/subgraphs/name/harmony/battleship3',
   gameContractAddress: '0x7a7b3a1f88d07d4aaa481474ee1853d715c1a70e',
 };
 
